f51629618f5548243a59abf059344b17f4626b49
Karan Mehta <mehtakaran165@gmail.com>
2026-04-19 11:15:01 +0530

    Fixing extension

--- extension/config.js
+++ extension/config.js
@@ -1,3 +1,3 @@
 // TODO: If you deploy the backend to Render, update this URL to your deployed Render URL.
 // Example: const API_BASE_URL = 'https://cybershield-backend-mrr3.onrender.com';
-const API_BASE_URL = 'https://cybershield-backend-mrr3.onrender.com';
+var API_BASE_URL = 'https://cybershield-backend-mrr3.onrender.com';
